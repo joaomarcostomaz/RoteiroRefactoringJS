0a99770db82012c052e03c35ca8d9f58adbf92b2
João Marcos Tomáz Silva Campos <joaomarcoscampos@dcc.ufmg.br>
2024-12-04 18:07:53 -0300

    Commit 4 - Separando Apresentação dos Cálculos

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const { readFileSync } = require('fs');
 function gerarFaturaStr (fatura, pecas) {
     let totalFatura = 0;
     let creditos = 0;
-    let faturaStr = `Fatura ${fatura.cliente}\n`;
     function formatarMoeda(valor) {
       return new Intl.NumberFormat("pt-BR",
         { style: "currency", currency: "BRL",
@@ -43,22 +42,26 @@ function gerarFaturaStr (fatura, pecas) {
         creditos += Math.floor(apre.audiencia / 5);
       return creditos;
     }
-
+    function calcularTotalFatura() {
+      return fatura.apresentacoes.reduce(
+        (total, apre) => total + calcularTotalApresentacao(apre),
+        0
+      );
+    }
+    function calcularTotalCreditos() {
+      return fatura.apresentacoes.reduce(
+        (total, apre) => total + calcularCredito(apre),
+        0
+      );
+    }
+    let faturaStr = `Fatura ${fatura.cliente}\n`;
     for (let apre of fatura.apresentacoes) {
-      //const peca = getPeca(apre);
-      let total = calcularTotalApresentacao(apre);
-    
-      // créditos para próximas contratações
-      creditos += calcularCredito(apre);
-    
-        // mais uma linha da fatura
-        faturaStr += `  ${getPeca(apre).nome}: ${formatarMoeda(total)} (${apre.audiencia} assentos)\n`;
-        totalFatura += total;
-    } 
-    faturaStr += `Valor total: ${formatarMoeda(totalFatura)}\n`;
-    faturaStr += `Créditos acumulados: ${creditos} \n`;
-    return faturaStr;
-  }
+      faturaStr += `  ${getPeca(apre).nome}: ${formatarMoeda(calcularTotalApresentacao(apre))} (${apre.audiencia} assentos)\n`;
+    }
+    faturaStr += `Valor total: ${formatarMoeda(calcularTotalFatura())}\n`;
+    faturaStr += `Créditos acumulados: ${calcularTotalCreditos()} \n`;
+      return faturaStr;
+    }
 
 const faturas = JSON.parse(readFileSync('./faturas.json'));
 const pecas = JSON.parse(readFileSync('./pecas.json'));
